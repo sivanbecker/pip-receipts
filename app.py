70887e8e2be0516fb07f4c87b270cf1781c3fc51
Sivan Becker <sivanbecker@gmail.com>
2020-11-20 15:34:57 +0200

    add create_app and pytest-postgres requirment

--- app.py
+++ app.py
@@ -9,20 +9,32 @@ SQLALCHEMY_DATABASE_URI = os.environ.get(
 )
 SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS")
 
-app = Flask(__name__)
-app.config["DEBUG"] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
-app.config["RECEIPTS_URL"] = os.environ.get("RECEIPTS_URL")
-api = Api(app)
 
-api.add_resource(ReceiptResource, "/receipt/<string:uniq_str>")
-api.add_resource(ReceiptListResource, "/receipts")
+def create_app():
+    _app = Flask(__name__)
+    _app.config["DEBUG"] = True
+    _app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+    _app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
+    _app.config["RECEIPTS_URL"] = os.environ.get("RECEIPTS_URL")
+    api = Api(_app)
+
+    api.add_resource(ReceiptResource, "/receipt/<string:uniq_str>")
+    api.add_resource(ReceiptListResource, "/receipts")
+
+    @_app.route("/index")
+    @_app.route("/")
+    def index():  # pylint: disable=unused-variable
+        return "Welcome to Receipts App"
+
+    return _app
+
 
 if __name__ == "__main__":
+
     from db import db
     from schemas.receipt_schema import ma
 
+    app = create_app()
     db.init_app(app)
     with app.app_context():
         db.create_all()
