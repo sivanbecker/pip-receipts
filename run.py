71e3b7cb43dd1c1b0cd14744cceec5c30560305b
Sivan Becker <sivanbecker@gmail.com>
2020-11-20 20:33:26 +0200

    fix importing in run.py

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
-from app import app
-from db import db
+from app import create_app  # pylint: disable=import-error
+from db import db  # pylint: disable=import-error
 from schemas.provider import ma
 
-db.init_app(app)
-ma.init_app(app)
+_app = create_app()
+db.init_app(_app)
+ma.init_app(_app)
 
 
-@app.before_first_request
+@_app.before_first_request
 def create_tables():
     db.create_all()
